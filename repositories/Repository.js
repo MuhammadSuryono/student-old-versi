b9b3f1970b148f1be8d04848f5068cbc6a554bf1
‘Mohamad <mwahyudin@pm.me>
2022-11-25 01:21:27 +0700

    add & modify for data statistics

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -4,6 +4,7 @@ import DecorationRepository from '~/repositories/decorationRepository'
 import SkillcardRepository from '~/repositories/SkillcardRepository'
 import ModuledRepository from '~/repositories/moduleRepository'
 import FactionRepository from '~/repositories/factionRepository'
+import StatisticRepository from '~/repositories/statisticRepository'
 
 export default $axios => ({
   user: UserRepository($axios),
@@ -11,5 +12,6 @@ export default $axios => ({
   decoration: DecorationRepository($axios),
   skillcard: SkillcardRepository($axios),
   module: ModuledRepository($axios),
-  faction: FactionRepository($axios)
+  faction: FactionRepository($axios),
+  statistic: StatisticRepository($axios)
 })
